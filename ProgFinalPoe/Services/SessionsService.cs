c38102bbd1708d507c900dabc2e1ea66142fb23c
ZodeaJ <ST10440733@imconnect.edu.za>
2025-11-20 13:48:23 +0200

    Commit 5 - Implementation of Sessions
    What actually changed:
    Added a folder called "Services" and a class in the folder called "SessionsService" containing the following methods: public void SetUserSession(User user){}
    public bool IsUserLoggedIn(){}
    public string GetUserRole(){}
    public int GetUserId(){}
    public void ClearSession(){}

---
+++ ProgFinalPoe/Services/SessionsService.cs
@@ -0,0 +1,42 @@
+﻿using ProgFinalPoe.Models;
+
+namespace ProgFinalPoe.Services
+{
+    public class SessionService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public SessionService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void SetUserSession(User user)
+        {
+            _httpContextAccessor.HttpContext.Session.SetInt32("UserId", user.UserId);
+            _httpContextAccessor.HttpContext.Session.SetString("Username", user.Username);
+            _httpContextAccessor.HttpContext.Session.SetString("Role", user.Role);
+            _httpContextAccessor.HttpContext.Session.SetString("Name", user.Name);
+        }
+
+        public bool IsUserLoggedIn()
+        {
+            return _httpContextAccessor.HttpContext.Session.GetInt32("UserId") != null;
+        }
+
+        public string GetUserRole()
+        {
+            return _httpContextAccessor.HttpContext.Session.GetString("Role");
+        }
+
+        public int GetUserId()
+        {
+            return _httpContextAccessor.HttpContext.Session.GetInt32("UserId") ?? 0;
+        }
+
+        public void ClearSession()
+        {
+            _httpContextAccessor.HttpContext.Session.Clear();
+        }
+    }
+}
--- ProgFinalPoe/obj/Debug/net9.0/ProgFinalPoe.AssemblyInfo.cs
+++ ProgFinalPoe/obj/Debug/net9.0/ProgFinalPoe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgFinalPoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b83122a969768dd0e25f3f58aa79393819c5723")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgFinalPoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgFinalPoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
